ae2567586f962d5396de6d23f1b5d233ca771da4
Edward Zhu <ezhu010@ucr.edu>
2024-04-04 21:03:39 -0700

    added batch size to test

--- local_traniner/model/test.py
+++ local_traniner/model/test.py
@@ -32,42 +32,41 @@ if __name__ == '__main__':
 
     test_path = '/home/edwardzhu0211/CS598-DLH/local_traniner/input/test'
     testset = dataset.SARS(root=test_path, is_train=False)
+    
     testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
-                                                shuffle=False, num_workers=2, drop_last=False)
+                                            shuffle=False, num_workers=2, drop_last=False)
     creterion = torch.nn.CrossEntropyLoss()
 
 
-
-    for i, data in enumerate(testloader):
-
-    # =============================================================================
-    #             if i < 1:
-    #                 continue
-    # =============================================================================
-        with torch.no_grad():
-            img, label, img_raw = data[0].cuda(), data[1].cuda(), data[2]
-            batch_size = img.size(0)
-            _, concat_logits, _, _, _ = net(img, img_raw, False, False)
-            # calculate loss
-            concat_loss = creterion(concat_logits, label)
-            # calculate accuracy
-            _, concat_predict = torch.max(concat_logits, 1)
-            auc_label_lst += list(label.data.cpu().numpy())
-            pred = torch.nn.Softmax(1)(concat_logits)
-            auc_pred_lst.append(pred.data.cpu().numpy())
-            people_lst.append(data[3])
-            file_name_lst += list(data[4])
-    # =============================================================================
-    #                 img_vis_lst.append(img_vis)
-    #                 anchor_lst.append(anchor)
-    # =============================================================================
-
-            total += batch_size
-            test_correct += torch.sum(concat_predict.data == label.data)
-            test_loss += concat_loss.item() * batch_size
-            progress_bar(i, len(testloader), 'eval test set')
-    test_acc = float(test_correct) / total
-    test_loss = test_loss / total
-    print(f'auc: {roc_auc_score(auc_label_lst, np.concatenate(auc_pred_lst, 0)[:, 1]):.4f}')
-    print(test_acc)
-    print(test_loss)
+    for epoch in range(start_epoch, 100):
+        for i, data in enumerate(testloader):
+        # =============================================================================
+        #             if i < 1:
+        #                 continue
+        # =============================================================================
+            with torch.no_grad():
+                img, label, img_raw = data[0].cuda(), data[1].cuda(), data[2]
+                batch_size = img.size(0)
+                _, concat_logits, _, _, _ = net(img, img_raw, False, False)
+                # calculate loss
+                concat_loss = creterion(concat_logits, label)
+                # calculate accuracy
+                _, concat_predict = torch.max(concat_logits, 1)
+                auc_label_lst += list(label.data.cpu().numpy())
+                pred = torch.nn.Softmax(1)(concat_logits)
+                auc_pred_lst.append(pred.data.cpu().numpy())
+                people_lst.append(data[3])
+                file_name_lst += list(data[4])
+        # =============================================================================
+        #                 img_vis_lst.append(img_vis)
+        #                 anchor_lst.append(anchor)
+        # =============================================================================
+                total += batch_size
+                test_correct += torch.sum(concat_predict.data == label.data)
+                test_loss += concat_loss.item() * batch_size
+                progress_bar(i, len(testloader), 'eval test set')
+        test_acc = float(test_correct) / total
+        test_loss = test_loss / total
+        print(f'auc: {roc_auc_score(auc_label_lst, np.concatenate(auc_pred_lst, 0)[:, 1]):.4f}')
+        print(f'Test acc: {test_acc}')
+        print(f'Test loss; {test_loss}')
