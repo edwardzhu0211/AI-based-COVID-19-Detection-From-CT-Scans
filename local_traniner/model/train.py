4903069901aa3fa670e0e2b9b63a9269b7fec1a5
Edward Zhu <ezhu010@ucr.edu>
2024-04-16 19:17:21 -0700

    added testing back to train loop

--- local_traniner/model/train.py
+++ local_traniner/model/train.py
@@ -12,7 +12,8 @@ import pickle
 
 
 if __name__ == '__main__':
-    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    # ======= reading the dataset =============== 
+    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
     start_epoch = 1
     save_dir = os.path.join(save_dir, datetime.now().strftime('%Y%m%d_%H%M%S'))
     if os.path.exists(save_dir):
@@ -20,9 +21,6 @@ if __name__ == '__main__':
     os.makedirs(save_dir)
     logging = init_log(save_dir)
     _print = logging.info
-
-    # read dataset
-
     train_path = '../input/train/'
     val_path = '../input/val/'
     test_path = '../input/test/'
@@ -35,33 +33,28 @@ if __name__ == '__main__':
                                             shuffle=False, num_workers=2, drop_last=False)
     valloader = torch.utils.data.DataLoader(valset, batch_size=BATCH_SIZE,
                                             shuffle=False, num_workers=2, drop_last=False)
+    # ======= reading the dataset =============== 
 
+    # ======= model =============== 
     n_class = 2
-    # define model
     net = model.attention_net(topN=PROPOSAL_NUM, n_class=n_class)
     if resume:
         ckpt = torch.load(resume)
         net.load_state_dict(ckpt['net_state_dict'])
         start_epoch = ckpt['epoch'] + 1
-
     creterion = torch.nn.CrossEntropyLoss()
-
-    # define optimizers
     raw_parameters = list(net.pretrained_model.parameters())
     part_parameters = list(net.proposal_net.parameters())
     concat_parameters = list(net.concat_net.parameters())
     partcls_parameters = list(net.partcls_net.parameters())
-
     raw_optimizer = torch.optim.SGD(raw_parameters, lr=LR, momentum=0.9, weight_decay=WD)
     concat_optimizer = torch.optim.SGD(concat_parameters, lr=LR, momentum=0.9, weight_decay=WD)
     part_optimizer = torch.optim.SGD(part_parameters, lr=LR, momentum=0.9, weight_decay=WD)
     partcls_optimizer = torch.optim.SGD(partcls_parameters, lr=LR, momentum=0.9, weight_decay=WD)
-
     schedulers = [MultiStepLR(raw_optimizer, milestones=[60, 100], gamma=0.1),
                 MultiStepLR(concat_optimizer, milestones=[60, 100], gamma=0.1),
                 MultiStepLR(part_optimizer, milestones=[60, 100], gamma=0.1),
                 MultiStepLR(partcls_optimizer, milestones=[60, 100], gamma=0.1)]
-
     if resume:
         ckpt = torch.load(resume)
         net.pretrained_model.load_state_dict({layer.replace('pretrained_model.', ''):ckpt['net_state_dict'][layer] 
@@ -69,16 +62,15 @@ if __name__ == '__main__':
         start_epoch = ckpt['epoch'] + 1
     net = net.cuda()
     net = DataParallel(net)
+    # ======= model =============== 
 
+    # ======= training =============== 
     skip_epoch = 0
     for epoch in range(start_epoch, 2):
         if epoch > skip_epoch:
             add = True
         else:
             add = False
-
-
-        # begin training
         _print('--' * 50)
         net.train()
         train_correct = 0
@@ -115,7 +107,7 @@ if __name__ == '__main__':
             
         print(float(train_correct) / total)
         pickle.dump(net, open('./model.pkl', 'wb'))
-        if epoch % SAVE_FREQ == 0 :#and epoch > 20:
+        if epoch % SAVE_FREQ == 0 :
             train_loss = 0
             train_correct = 0
             total = 0
@@ -158,65 +150,58 @@ if __name__ == '__main__':
             np.save('./train_label.npy', np.array(auc_label_lst))
             np.save('./train_people.npy', np.concatenate(people_lst, 0))
             np.save('./train_file_name.npy', np.concatenate(file_name_lst, 0))
-        # evaluate on test set
-    #         test_loss = 0
-    #         test_correct = 0
-    #         total = 0
-    #         auc_label_lst = []
-    #         auc_pred_lst = []
-    #         people_lst = []
-    #         img_vis_lst = []
-    #         file_name_lst = []
-    #         anchor_lst = []
-    #         for i, data in enumerate(testloader):
-    # # =============================================================================
-    # #             if i < 1:
-    # #                 continue
-    # # =============================================================================
-    #             with torch.no_grad():
-    #                 img, label, img_raw = data[0].cuda(), data[1].cuda(), data[2]
-    #                 batch_size = img.size(0)
-    #                 _, concat_logits, _, _, _ = net(img, img_raw, add, False)
-    #                 # calculate loss
-    #                 concat_loss = creterion(concat_logits, label)
-    #                 # calculate accuracy
-    #                 _, concat_predict = torch.max(concat_logits, 1)
-    #                 auc_label_lst += list(label.data.cpu().numpy())
-    #                 pred = torch.nn.Softmax(1)(concat_logits)
-    #                 auc_pred_lst.append(pred.data.cpu().numpy())
-    #                 people_lst.append(data[3])
-    #                 file_name_lst += list(data[4])
-    # # =============================================================================
-    # #                 img_vis_lst.append(img_vis)
-    # #                 anchor_lst.append(anchor)
-    # # =============================================================================
+            test_loss = 0
+            test_correct = 0
+            total = 0
+            auc_label_lst = []
+            auc_pred_lst = []
+            people_lst = []
+            img_vis_lst = []
+            file_name_lst = []
+            anchor_lst = []
 
-    #                 total += batch_size
-    #                 test_correct += torch.sum(concat_predict.data == label.data)
-    #                 test_loss += concat_loss.item() * batch_size
-    #                 progress_bar(i, len(testloader), 'eval test set')
-    #         test_acc = float(test_correct) / total
-    #         test_loss = test_loss / total
-    #         _print(
-    #             'epoch:{} - test loss: {:.3f} and test acc: {:.3f} total sample: {}'.format(
-    #                 epoch,
-    #                 test_loss,
-    #                 test_acc,
-    #                 total))
-            
-            
-    #         print(f'auc: {roc_auc_score(auc_label_lst, np.concatenate(auc_pred_lst, 0)[:, 1]):.4f}')
-    #         np.save('./test_pred.npy', np.concatenate(auc_pred_lst, 0))
-    #         np.save('./test_label.npy', np.array(auc_label_lst))
-    #         np.save('./test_people.npy', np.concatenate(people_lst, 0))
-    #         np.save('./test_file_name.npy', np.array(file_name_lst))
-            
-    # =============================================================================
-    #         np.save('./test_anchor_lst.npy', np.concatenate(anchor_lst, 0))
-    #         np.save('./test_vis.npy', np.concatenate(img_vis_lst, 0))
-    #         assert 0
-    # =============================================================================
-        # save model
+
+            for i, data in enumerate(testloader):
+                # =============================================================================
+                #             if i < 1:
+                #                 continue
+                # =============================================================================
+                    with torch.no_grad():
+                        img, label, img_raw = data[0].cuda(), data[1].cuda(), data[2]
+                        batch_size = img.size(0)
+                        _, concat_logits, _, _, _ = net(img, img_raw, False, False)
+                        # calculate loss
+                        concat_loss = creterion(concat_logits, label)
+                        # calculate accuracy
+                        _, concat_predict = torch.max(concat_logits, 1)
+                        auc_label_lst += list(label.data.cpu().numpy())
+                        pred = torch.nn.Softmax(1)(concat_logits)
+                        auc_pred_lst.append(pred.data.cpu().numpy())
+                        people_lst.append(data[3])
+                        file_name_lst += list(data[4])
+                # =============================================================================
+                #                 img_vis_lst.append(img_vis)
+                #                 anchor_lst.append(anchor)
+                # =============================================================================
+                        total += batch_size
+                        test_correct += torch.sum(concat_predict.data == label.data)
+                        test_loss += concat_loss.item() * batch_size
+                        progress_bar(i, len(testloader), 'eval test set')
+            test_acc = float(test_correct) / total
+            test_loss = test_loss / total
+            _print(
+            'epoch:{} - test loss: {:.3f} and test acc: {:.3f} total sample: {}'.format(
+                epoch,
+                test_loss,
+                test_acc,
+                total))
+
+            print(f'auc: {roc_auc_score(auc_label_lst, np.concatenate(auc_pred_lst, 0)[:, 1]):.4f}')
+            np.save('./test_pred.npy', np.concatenate(auc_pred_lst, 0))
+            np.save('./test_label.npy', np.array(auc_label_lst))
+            np.save('./test_people.npy', np.concatenate(people_lst, 0))
+            np.save('./test_file_name.npy', np.array(file_name_lst))
+
             net_state_dict = net.module.state_dict()
             if not os.path.exists(save_dir):
                 os.mkdir(save_dir)
@@ -224,11 +209,8 @@ if __name__ == '__main__':
                 'epoch': epoch,
                 'train_loss': train_loss,
                 'train_acc': train_acc,
-                # 'test_loss': test_loss,
-                # 'test_acc': test_acc,
                 'net_state_dict': net_state_dict},
                 os.path.join(save_dir, '%03d.ckpt' % epoch))
-    # =============================================================================
-    #         assert 0
-    # =============================================================================
     print('finishing training')
+# ======= training =============== 
+
